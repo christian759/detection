814b19e3d035d290e286079af1b22f726d2ce699
CEO1 <131156880+christian759@users.noreply.github.com>
2024-05-26 14:56:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ with st.sidebar:
     st.markdown("##")
 
     options = st.multiselect(
-    'Select objects you want to detect',
+    'Select objects you want to detect (OPTIONAL)',
     ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',  'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 
      'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
     )
